2bbe5888861a60edecd6b9eeae3839c146120ba3
samir-hussein <samirhussein274@gmail.com>
2025-04-18 17:52:49 +0200

    fix date

--- resources/views/dashboard/welcome.blade.php
+++ resources/views/dashboard/welcome.blade.php
@@ -322,15 +322,16 @@
             },
             plotOptions: {
                 bar: {
-                    horizontal: true
+                    horizontal: false
                 }
             },
             dataLabels: {
                 enabled: true
             },
             stroke: {
-                width: 1,
-                colors: ['#fff']
+                show: true,
+                width: 4,
+                colors: ['transparent']
             },
             xaxis: {
                 categories: JSON.parse('{!! json_encode($categories) !!}'),
